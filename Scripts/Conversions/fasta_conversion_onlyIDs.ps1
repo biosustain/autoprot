46959c0ddfce076295bb568b43e2641dde8164b0
ShannaraTP <shannatp@xs4all.nl>
2023-01-28 01:49:46 -0800

    Improved selection of ID from FASTA headers

--- Scripts/Conversions/fasta_conversion_onlyIDs.ps1
+++ Scripts/Conversions/fasta_conversion_onlyIDs.ps1
@@ -3,7 +3,7 @@ $OutputFilePath = Join-Path $OutputDirPath ($fastaName + "_onlyIDs.fasta")
 $Sequences = Get-Content -Path $InputFilePath
 $Sequences = $Sequences | ForEach-Object {
     if ($_ -match "^>") {
-        $ID = $_.SubString($_.IndexOf("|") + 1, $_.LastIndexOf("|") - $_.IndexOf("|") - 1)
+        $ID = $_.SubString($_.IndexOf("|") + 1, $_.IndexOf("|",$_.IndexOf("|") + 1) - $_.IndexOf("|") - 1)
         ">" + $ID
     }
     else {
